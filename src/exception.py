69a9e200f5a9bedab07029a117eeb6e3277e1c5b
roshan9900 <roshansalunke91@gmail.com>
2025-10-15 11:42:34 +0530

    created exception and logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+import logging
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb = error_details.exc_info()
@@ -10,8 +10,10 @@ def error_message_details(error,error_details:sys):
     return error_message
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_details)
 
     def __str__(self):
         return self.error_message
+    
+
